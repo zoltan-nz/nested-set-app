c9b15a27a4235c2a1a209a48cd46d8b7057c027d
Zoltan <zdebre@gmail.com>
2013-12-24 23:59:50 +0000

    clean

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-MechanizeSandbox::Application.config.secret_token = '675ed232b86f6999f7f31fb443f55847effa3af97b57abdc071f0bb3948d8d6149ed64b1d563b88916a9dcde5a2b04465bb6226f6a1a38d38f400cf642723442'
